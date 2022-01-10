5def09c3aad1252b58ca3a718e000b6f4c2db6f0
Guarize <pedroguarize@hotmail.com>
2022-01-10 11:39:58 -0300

    [feat] - transactions structure

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -19,14 +19,18 @@ const ServiceCard = ({ color, title, icon, subtitle }) => (
 const Services = () => {
   return (
     <div className="flex flex-col md:flex-row w-full justify-center items-center gradient-bg-services">
-      <div className="xmf:ml-60 flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
+      <div className="xmf:ml-40 flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
         <div className="flex-1 flex flex-col justify-start items-start">
           <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient">
             Services that we <br /> continue to improve
           </h1>
+          <p className="text-left my-2 text-white font-light md:w-9/12 w-11/12 text-base">
+            The best choice for buying and selling your crypto assets, with the
+            various super friendly services we offer
+          </p>
         </div>
       </div>
-      <div className="flex-1 flex flex-col justify-start items-center">
+      <div className="flex-1 xmf:-ml-60 flex flex-col justify-start items-center">
         <ServiceCard
           color="bg-[#2952e3]"
           title="Security Guaranteed"
